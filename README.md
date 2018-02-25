31b510af9a1e2dbb6ae832dc3b6ea35f77a88752
Iain J McCallum <ijmccallum@hotmail.co.uk>
2018-02-25 09:48:41 -0500

    setting up the starter - watch problems

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # msg-starter
 A starter repo for the mini site generator
+
+builds to ./docs for use in github pages, or use netlify and point it to said docs folder.
---
+++ src/index.page.js
@@ -0,0 +1,22 @@
+module.exports = function(){
+    return html`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Mini Site Generator</title>
+            <link href="main.css" rel="stylesheet">
+        </head>
+        <body class="text-center">
+
+            <section class="screen-height clr-white bg-black center-section">
+                <h1>Mini Site Generator</h1>
+                <p><em>It's Just <span class="clr-yellow">JavaScript</span></em></p>
+            </section>
+
+        </body>
+        </html>
+    `;
+}
