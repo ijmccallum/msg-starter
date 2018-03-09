9965b4f7606b12341eb9bac6386263e78e6d608d
ijmccallum <ijmccallum@hotmail.co.uk>
2018-03-08 20:29:14 -0500

    content styles

--- src/markup/index.page.js
+++ src/markup/index.page.js
@@ -2,36 +2,7 @@ let baseLayout = require("./layouts/base.js");
 
 module.exports = function() {
     let pageContent = html`
-        <section class="clr-white bg-black pv5 ph4">
-            <div class="flex-row">
-                <div class="w-20">     
-                </div>
-                <div>
-                    <p class="ma0">Iain's</p>
-                    <h1 class="h2 mt0">MSG Starter Kit</h1>
-                </div>
-            </div>
-            <div class="flex-row">
-                <div class="w-20 pr3">
-                    <div class="pb2">Input with icon</div>
-                </div>
-                <div class="w-20 pr3">
-                    <div class="pb2">Breakpoint columns</div>
-                    <div class="pb2">Grids</div>
-                </div>
-                <div class="w-20 pr3">     
-                    <div class="pb2">Stack to horizontal</div>
-                    <div class="pb2">Priority Plus</div>
-                    <div class="pb2">Horizontal Scroller</div>
-                </div>
-                <div class="w-20 pr3">
-                    <div class="pb2">Popin</div>
-                    <div class="pb2">Popout</div>
-                    <div class="pb2">Modal</div>
-                    <div class="pb2">Drawer</div>
-                </div>
-            </div>
-        </section>
+        
         
         <section>
                 <p>A personalised collection of the little things I've picked up from around the industry, the nifty things that give a project that extra polish.</p>
--- src/markup/layouts/base.js
+++ src/markup/layouts/base.js
@@ -20,6 +20,44 @@ module.exports = function(content) {
                 <link href="main.css" rel="stylesheet">
             </head>
             <body>
+                <section class="clr-white bg-black pv5 ph4">
+                    <div class="flex-row">
+                        <div class="w-20">     
+                        </div>
+                        <div>
+                            <p class="ma0">Iain's</p>
+                            <h1 class="h2 mt0">MSG Starter Kit</h1>
+                        </div>
+                    </div>
+                    <div class="flex-row">
+                        <div class="w-20 pr3">
+                            <div class="pb2">Input with icon</div>
+                        </div>
+                        <div class="w-20 pr3">
+                            <div class="pb2">Breakpoint columns</div>
+                            <div class="pb2">Grids</div>
+                        </div>
+                        <div class="w-20 pr3">     
+                            <div class="pb2">Stack to horizontal</div>
+                            <div class="pb2">Priority Plus</div>
+                            <div class="pb2">Horizontal Scroller</div>
+                        </div>
+                        <div class="w-20 pr3">
+                            <div class="pb2">Popin</div>
+                            <div class="pb2">Popout</div>
+                            <div class="pb2">Modal</div>
+                            <div class="pb2">Drawer</div>
+                        </div>
+                        <div class="w-20 pr3">
+                            <div class="pb2">Headings</div>
+                            <div class="pb2">Paragraphs</div>
+                            <div class="pb2">Links</div>
+                            <div class="pb2">Lists</div>
+                            <div class="pb2">Tables</div>
+                            <div class="pb2">Other els</div>
+                        </div>
+                    </div>
+                </section>
                 ${content}
             </body>
         </html>
