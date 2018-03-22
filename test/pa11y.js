923f619f3929ad65205dc5aafc5f00bd1c69a804
Iain J McCallum <ijmccallum@hotmail.co.uk>
2018-03-22 07:23:28 -0400

    styling the select arrow

--- test/pa11y.js
+++ test/pa11y.js
@@ -1,9 +1,11 @@
 "use strict";
 const pa11y = require("pa11y");
-const cheerio = require('cheerio');
-const request = require('request');
-const entryUrl = "https://ijmccallum.github.io/";
+const cheerio = require("cheerio");
+const fs = require("fs");
+const path = require("path");
+const entryUrl = "https://www.americanstandard-us.com/";
 const pageLimit = 10;
+const logDir = makePathAbsolute("./test/pa11ylog");
 /*
 I'm pretty sure recursion in JS isn't yet a solved problem (the growing stack issue),
 so instead I've gone for a slightly more complicated iterative approach, 
@@ -21,7 +23,8 @@ async function begin(url) {
     console.log(1);
     while (untestedPages.length > 0 && testedPages.length <= pageLimit) {
         console.log(2);
-        untestedPages.forEach(page => {
+        //TODO: await this, otherwise it goes MAD!
+        for (const page of untestedPages) {
             console.log(3);
             if (!isPageInMap(page, testedPages)) {
                 console.log(4);
@@ -38,40 +41,65 @@ async function begin(url) {
                 let links = await getLinksFromPage(page.url);
                 //add them to the untested pages map
                 links.forEach(link => {
-                    //but only if they aren't in the tested pages map
-                    if (!isPageInMap({ url: link }, testedPages)) {
-                        untestedPages.push({
-                            url: link,
-                            results: {}
-                        });
+                    //only child page
+                    if (link.indexOf(entryUrl) == -1) {
+                        console.log(`Not testing: ${link}`);
+                        return;
                     }
+                    //don't retest pages
+                    if (isPageInMap({ url: link }, testedPages)) {
+                        return;
+                    }
+                    untestedPages.push({
+                        url: link,
+                        results: {}
+                    });
                 });
             }
             //remove from untested pages (if there are dupes, they will all be removed)
             untestedPages = removePageFromMap(page, untestedPages);
-        });
+        }
     }
+    console.log("testedPages", testedPages);
+    //TODO: save results, into pa11y results folder then use that to drive a UI?
+    if (!isDirectory(logDir)) {
+        console.log("Making Dir");
+        ensureFolderAddress(logDir);
+    }
+    var logFileName = generateLogFilename(logDir);
+    console.log("logFileName", logFileName);
+    fs.writeFile(logFileName, JSON.stringify(testedPages), function(err) {
+        console.log("log fail", err);
+    });
+}
+
+function generateLogFilename(logDir) {
+    let logAddress = logDir;
+    if (logAddress.slice(-1) != "/") {
+        logAddress += "/";
+    }
+    return (logAddress += Date.now() + ".json");
 }
 
 function getLinksFromPage(url) {
     return new Promise((resolve, reject) => {
-        const lib = url.startsWith('https') ? require('https') : require('http');
-        lib.get(url, (response) => {
+        const lib = url.startsWith("https") ? require("https") : require("http");
+        lib.get(url, response => {
             // handle http errors
             if (response.statusCode < 200 || response.statusCode > 299) {
-               resolve([]);
+                resolve([]);
             }
             // temporary data holder
             const body = [];
             // on every content chunk, push it to the data array
-            response.on('data', (chunk) => body.push(chunk));
+            response.on("data", chunk => body.push(chunk));
             // we are done, resolve promise with those joined chunks
-            response.on('end', () => {
-                let $ = cheerio.load(body.join(''));
+            response.on("end", () => {
+                let $ = cheerio.load(body.join(""));
                 let returnLinks = [];
-                links = $('a'); //jquery get all hyperlinks
-                $(links).each(function(i, link){
-                    returnLinks.push($(link).attr('href'));
+                let links = $("a"); //jquery get all hyperlinks
+                $(links).each(function(i, link) {
+                    returnLinks.push($(link).attr("href"));
                 });
                 resolve(returnLinks);
             });
@@ -80,7 +108,7 @@ function getLinksFromPage(url) {
 }
 
 function isPageInMap(testPage, pageMap) {
-    pageMap.array.forEach(mapPage => {
+    pageMap.forEach(mapPage => {
         if (mapPage.url == testPage.url) {
             return true;
         }
@@ -98,6 +126,30 @@ function removePageFromMap(removalPage, pageMap) {
     return returnableageMap;
 }
 
+/**
+ * Takes an address
+ * Returns true if that address points to a folder
+ */
+function isDirectory(sourceAddres) {
+    return fs.lstatSync(sourceAddres).isDirectory();
+}
+
+function makePathAbsolute(relativePath) {
+    const absPath = path.resolve(relativePath);
+    return absPath;
+}
+
+/**
+ * Takes a folder address
+ * Makes it if it doesn't exist already
+ */
+function ensureFolderAddress(folderAddress) {
+    if (!fs.existsSync(folderAddress)) {
+        fs.mkdirSync(folderAddress);
+    }
+    return folderAddress;
+}
+
 //return a promise / resolve to a results object
 function pa11yPage(url) {
     return new Promise((resolve, reject) => {
@@ -108,11 +160,13 @@ function pa11yPage(url) {
                     error: console.error,
                     info: console.log
                 }
-            }).then(results => {
-                resolve(results);
-            }).catch(err => {
-                resolve(err);
-            });
+            })
+                .then(results => {
+                    resolve(results);
+                })
+                .catch(err => {
+                    resolve(err);
+                });
         } catch (err) {
             resolve(err);
         }
